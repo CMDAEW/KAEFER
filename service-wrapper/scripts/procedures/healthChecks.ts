3a5d587aa121751b9d452fa924dca10c5bfcab1d
CMDAEW <92660496+CMDAEW@users.noreply.github.com>
2024-07-05 17:59:37 +0200

    Delete service-wrapper directory

--- service-wrapper/scripts/procedures/healthChecks.ts
+++
@@ -1,7 +0,0 @@
-import { types as T, healthUtil } from "../deps.ts";
-
-export const health: T.ExpectedExports.health = {
-  async "web-ui"(effects, duration) {
-    return healthUtil.checkWebUrl("http://invoicing.embassy:80")(effects, duration).catch(healthUtil.catchError(effects))
-  },
-};
